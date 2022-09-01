f2cc2b7ce2c42118a798f2cbc5178b667755d3ef
Trard <trardone@gmail.com>
2022-09-01 14:07:42 +0300

    added code docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,69 @@
+//! This library provides a simple procedural macro for fast implementing error methods in
+//! [serde::Serializer](https://docs.rs/serde/latest/serde/trait.Serializer.html) trait.
+
+/// Implements error method for [serde::Serializer](https://docs.rs/serde/latest/serde/trait.Serializer.html)
+/// 
+/// # Examples
+///
+/// ```
+/// use impl_serde_serialize_error::impl_serde_serialize_error;
+/// use serde::ser;
+/// # use thiserror::Error;
+/// # 
+/// # #[derive(Debug, Error)]
+/// # enum SerializationError {
+/// #     #[error("Cannot serialize value")]
+/// #     CannotSerialize,
+/// #     #[error("Custom({0})")]
+/// #     Custom(String)
+/// # }
+/// # 
+/// # impl serde::ser::Error for SerializationError {
+/// #     fn custom<T>(msg:T) -> Self
+/// #     where T: std::fmt::Display
+/// #     {
+/// #         SerializationError::Custom(msg.to_string())
+/// #     }
+/// # }
+/// # 
+/// struct MySerializer;
+/// 
+/// impl ser::Serializer for MySerializer {
+/// #    type Ok = ();
+/// #    type Error = SerializationError;
+/// #
+/// #   type SerializeMap = ser::Impossible<Self::Ok, Self::Error>;
+/// #   type SerializeSeq = ser::Impossible<Self::Ok, Self::Error>;
+/// #   type SerializeStruct = ser::Impossible<Self::Ok, Self::Error>;
+/// #   type SerializeStructVariant = ser::Impossible<Self::Ok, Self::Error>;
+/// #   type SerializeTuple = ser::Impossible<Self::Ok, Self::Error>;
+/// #   type SerializeTupleStruct = ser::Impossible<Self::Ok, Self::Error>;
+/// #   type SerializeTupleVariant = ser::Impossible<Self::Ok, Self::Error>;
+/// #
+///     //for one type
+///     impl_serde_serialize_error!(SerializationError::CannotSerialize, bool);
+/// 
+///     //for many types
+///     impl_serde_serialize_error!(SerializationError::CannotSerialize, [bytes, i8, i16]);
+/// #
+/// # impl_serde_serialize_error!(
+/// #     SerializationError::CannotSerialize, [
+/// #         i32, i64,
+/// #         u8, u16, u32, u64,
+/// #         f32, f64,
+/// #         char,
+/// #         str,
+/// #         none, some, unit,
+/// #         unit_struct, unit_variant,
+/// #         newtype_struct, newtype_variant,
+/// #         seq, map,
+/// #         tuple, tuple_struct, tuple_variant,
+/// #         struct, struct_variant
+/// #     ]
+/// # );
+/// }
+/// 
+/// ```
 #[macro_export]
 macro_rules! impl_serde_serialize_error {
     ($error:expr, [$($type:ident),+]) => {
